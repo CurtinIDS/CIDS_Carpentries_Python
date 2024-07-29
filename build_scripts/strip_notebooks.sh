60bf4cc4947aa28ff75207d90eb7ec2509bd449b
catchSheep <81134674+catchSheep@users.noreply.github.com>
2024-07-29 04:14:55 +0000

    Removing old colab dir

--- build_scripts/strip_notebooks.sh
+++ build_scripts/strip_notebooks.sh
@@ -6,8 +6,10 @@ exec 1>&2
 # Clear outputs from all staged Jupyter notebooks
 for notebook in $(git diff --cached --name-only | grep '\.ipynb$')
 do
-    jupyter nbconvert --to notebook --ClearOutputPreprocessor.enabled=True --inplace "$notebook"
-    git add "$notebook"
+    if [ -f "$notebook" ]; then # only run if notebook exists, i.e. hasnt been git rmd
+        jupyter nbconvert --to notebook --ClearOutputPreprocessor.enabled=True --inplace "$notebook"
+        git add "$notebook"
+    fi
 done
 
 exit 0
